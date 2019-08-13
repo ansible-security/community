5187ae2939ee24bcaa920f246887cb210e4bfbf4
Adam Miller <admiller@redhat.com>
2019-08-12 21:52:16 -0500

    WIP - qradar_offense_note module
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- plugins/modules/qradar_offense_note.py
+++ plugins/modules/qradar_offense_note.py
@@ -14,34 +14,20 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = '''
 ---
 module: qradar_offense_note
-short_description: Manage the state of a QRadar Offense Note
+short_description: Create or update a QRadar Offense Note
 description:
-  - This module allows to create, delete, and update a QRadar Offense note
+  - This module allows to create a QRadar Offense note
 version_added: "2.9"
 options:
-  state:
-    description: Define state of the note: present or absent
-    required: false
-    choices: ["present", "absent"]
-    default: "present"
   offense_id:
     description:
       - Offense ID to operate on
     required: true
     type: int
-  note_id:
-    description: The note ID
-    required: false
-    type: int
   note_text:
     description: The note's text contents
-    required: false
+    required: true
     type: str
-notes:
-  - If both a note_id and note_text are provided, the note at note_id will be
-    updated to reflext the note_text provided.
-  - If only note_text is provided, the note containing that text will be
-    operated upon or a new one will be created, whichever is required.
 
 author: "Ansible Security Automation Team (https://github.com/ansible-security)
 '''
@@ -51,6 +37,13 @@ author: "Ansible Security Automation Team (https://github.com/ansible-security)
 #    - Name of Offense
 #   required: true
 #   type: str
+
+# FIXME - WOULD LIKE TO MANAGE STATE
+# state:
+#   description: Define state of the note: present or absent
+#   required: false
+#   choices: ["present", "absent"]
+#   default: "present"
 
 
 
@@ -88,10 +81,9 @@ def set_offense_values(module, qradar_request):
 def main():
 
     argspec = dict(
-        state=dict(required=False, choices=["present", "absent"], type='str', default="present"),
+#        state=dict(required=False, choices=["present", "absent"], type='str', default="present"),
         offense_id=dict(required=True, type='int'),
-        note_id=dict(required=False, type='int'),
-        note_text=dict(required=False, type='str'),
+        note_text=dict(required=True, type='str'),
     )
 
     module = AnsibleModule(
@@ -102,7 +94,7 @@ def main():
     qradar_request = QRadarRequest(
         module,
         headers={"Content-Type": "application/json", "Version": "9.1"},
-        not_rest_data_keys=['state', 'offense_id', 'note_id']
+        not_rest_data_keys=['state', 'offense_id']
     )
 
     #if module.params['name']:
@@ -110,60 +102,77 @@ def main():
     #    found_offense = qradar_request.get_by_path('api/siem/offenses?filter={0}'.format(module.params['name']))
     # FIXME - once this is sorted, add it to module_utils
 
-    if module.params['note_text'] and not module.params['note_id']:
-        found_notes = qradar_request.get_by_path(
-            'api/siem/offenses/{0}/notes?filter={1}'.format(
-                module.params['offense_id'],
-                quote('note_text="{0}"'.format(module.params['note_text'])),
-            )
+    found_notes = qradar_request.get_by_path(
+        'api/siem/offenses/{0}/notes?filter={1}'.format(
+            module.params['offense_id'],
+            quote('note_text="{0}"'.format(module.params['note_text'])),
         )
-    else:
-        found_notes = qradar_request.get_by_path(
-            'api/siem/offenses/{0}/notes/{1}'.format(
-                module.params['offense_id'],
-                module.params['note_id']
-            )
-        )
+    )
 
-    import q; q.q(found_notes)
+    #if module.params['state'] == 'present':
 
-    if module.params['state'] == 'present':
+    if found_notes:
+        # The note we want exists either by ID or by text name, verify
 
-        if found_notes:
-            # The note we want exists either by ID or by text name, verify
-            note = found_notes[0]
-
-            import q; q.q(note)
-
-        #module.exit_json(
-        #    msg="Successfully updated Offense Note ID: {0}".format(module.params['note_id']),
-        #    qradar_return_data=qradar_return_data,
-        #    changed=True
-        #)
+        note = found_notes[0]
+        if note['note_text'] == module.params['note_text']:
+            module.exit_json(msg="No changes necessary. Nothing to do.", changed=False)
         else:
-            # FIXME - Create The Note
+            if module.check_mode:
+                module.exit_json(msg="A change would have occured but did not because Check Mode", changed=True)
+
             qradar_return_data = qradar_request.post_by_path(
-                'api/siem/offenses/{0}/notes?{1}'.format(
+                'api/siem/offenses/{0}/notes?note_text={1}'.format(
                     module.params['offense_id'],
-                    quote('note_text={0}'.format(module.params['note_text'])),
+                    quote("{0}".format(module.params['note_text'])),
                 ),
                 data=False
             )
             module.exit_json(
-                msg="Successfully updated Offense Note ID: {0}".format(module.params['note_id']),
-                qradar_return_data=qradar_return_data,
-                changed=True
+                msg="Successfully created Offense Note ID: {0}".format(qradar_return_data['id']),
+                qradar_return_data=qradar_offense_note,
+                changed=False
             )
 
-        module.exit_json(msg="No changes necessary. Nothing to do.", changed=False)
+    else:
+        if module.check_mode:
+            module.exit_json(msg="A change would have occured but did not because Check Mode", changed=True)
 
-    if module.params['state'] == 'absent':
-        if not found_notes:
-            module.exit_json(msg="No changes necessary. Nothing to do.", changed=False)
-        else:
-            if module.check_mode:
-                # FIXME - put this somewhere useful with useful information
-                module.exit_json(msg="A change would have occured but did not because Check Mode", changed=True)
+        qradar_return_data = qradar_request.post_by_path(
+            'api/siem/offenses/{0}/notes?note_text={1}'.format(
+                module.params['offense_id'],
+                quote("{0}".format(module.params['note_text'])),
+            ),
+            data=False
+        )
+        module.exit_json(
+            msg="Successfully created Offense Note ID: {0}".format(qradar_return_data['id']),
+            qradar_return_data=qradar_return_data,
+            changed=True
+        )
+
+    module.exit_json(msg="No changes necessary. Nothing to do.", changed=False)
+
+    # FIXME FIXME FIXME - can we actually delete these via the REST API?
+    #if module.params['state'] == 'absent':
+    #    if not found_notes:
+    #        module.exit_json(msg="No changes necessary. Nothing to do.", changed=False)
+    #    else:
+    #        if module.check_mode:
+    #            module.exit_json(msg="A change would have occured but did not because Check Mode", changed=True)
+    #        # FIXME: fix the POST here to actually delete
+    #        qradar_return_data = qradar_request.post_by_path(
+    #            'api/siem/offenses/{0}/notes?note_text={1}'.format(
+    #                module.params['offense_id'],
+    #                quote("{0}".format(module.params['note_text'])),
+    #            ),
+    #            data=False
+    #        )
+    #        module.exit_json(
+    #            msg="Successfully created Offense Note ID: {0}".format(qradar_return_data['id']),
+    #            qradar_return_data=qradar_return_data,
+    #            changed=True
+    #        )
 
 
 if __name__ == '__main__':
